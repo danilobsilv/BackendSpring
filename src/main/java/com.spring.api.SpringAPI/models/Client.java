f96eb2845b26169e72977b507a51d3e7a990e89c
Danilo Bruno da Silva <96261259+danilobsilv@users.noreply.github.com>
2023-09-29 21:56:05 -0400

    Client

---
+++ src/main/java/com.spring.api.SpringAPI/models/Client.java
@@ -0,0 +1,74 @@
+package com.spring.api.SpringAPI.models;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+// I can use lombok instead of doing boilerplate codes
+// import lombok.data;
+
+@Entity  // to indicate that it is an entity and is being mapped to the database
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column()
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date registrationDate;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Client client)) return false;
+        return Id == client.Id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id);
+    }
+}
